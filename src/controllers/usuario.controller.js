1ef4d4517b1aa0bdef80a5a8c67739c846f93aa4
rico700 <rico700@hotmail.com>
2023-10-04 20:36:09 -0300

    criada base da classe p/ adicionarUsuario

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,3 +1,34 @@
 class UsuarioController {
-    
+  async adicionarUsuario(req, res) {
+    try {
+      const {
+        enderecoId,
+        nomeCompleto,
+        cpf,
+        dataNascimento,
+        telefone,
+        email,
+        senha,
+        criadoPor,
+        tipoUsuario,
+      } = req.body;
+      const usuario = await Usuario.create({
+        enderecoId,
+        nomeCompleto,
+        cpf,
+        dataNascimento,
+        telefone,
+        email,
+        senha,
+        criadoPor,
+        tipoUsuario,
+      });
+
+      return res
+      .status(201)
+      .send({ data: "Usuario salvo com sucesso!", usuario: usuario });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
