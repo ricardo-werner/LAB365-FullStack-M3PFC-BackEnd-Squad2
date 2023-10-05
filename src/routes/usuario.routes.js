cd29b0a9d95641af2764e207dd3be7549460a7b8
rico700 <rico700@hotmail.com>
2023-10-05 09:03:20 -0300

    criacao do usuario.routes.js

---
+++ src/routes/usuario.routes.js
@@ -0,0 +1,17 @@
+const {
+    adicionarUsuarioAdmin,
+    adicionarUsuarioComprador,
+    login,
+} = require("../controllers/usuario.controller");
+
+const { Router } = require("express");
+
+class UsuarioRouter {
+    routesFromUsuario () {
+        const usuarioRoutes = Router();
+
+        usuarioRoutes.post("usuario/login", login);
+        usuarioRoutes.post("usuario/cadastrar", adicionarUsuarioComprador);
+        usuarioRoutes.post("usuario/admin/cadastro", adicionarUsuarioAdmin);
+    }
+}
