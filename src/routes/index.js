b95dfe08096f648925fc1cd249d2785d7db5b582
ricardo-werner <wernerdive@gmail.com>
2023-10-05 00:44:04 -0300

    fix: delete routesFromUsuario

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,12 @@
 const { Router } = require('express')
-//const { routesFromUsuario } = require('./usuario.routes')
 const { routesFromVendas} = require('./vendas.routes')
 
 
 const routes = Router()
 
 routes.use('/api', [
-  //routesFromUsuario(),
   routesFromVendas(),
 
 ])
 
-module.exports = routes;
+module.exports = routes
