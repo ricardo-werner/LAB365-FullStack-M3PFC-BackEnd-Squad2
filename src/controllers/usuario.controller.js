e270dfcb0f80ed56099bf90b33414cfba6160680
rico700 <rico700@hotmail.com>
2023-10-04 21:00:32 -0300

    teste de campos obrigatorios

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -3,6 +3,15 @@ class UsuarioController {
     try {
       const {
         enderecoId,
+        cep,
+        logradouro,
+        numero,
+        bairro,
+        cidade,
+        estado,
+        complemento,
+        latitude,
+        longitude,
         nomeCompleto,
         cpf,
         dataNascimento,
@@ -12,6 +21,13 @@ class UsuarioController {
         criadoPor,
         tipoUsuario,
       } = req.body;
+
+// Verifica se os campos obrigatórios estão ausentes ou vazios
+if (!enderecoId || !cep || !logradouro || !numero || !bairro || !cidade || !estado || //campos de endereço obrigatórios
+!nomeCompleto || !cpf || !dataNascimento || !telefone || !email || !senha || !tipoUsuario) { //campos de usuario obrigatórios
+    return res.status(422).json({ error: 'Todos os campos obrigatórios devem ser preenchidos.' });
+  }
+
       const usuario = await Usuario.create({
         enderecoId,
         nomeCompleto,
