750580adbf58a0f69c6ac11c59090c36e5eeeb69
rico700 <rico700@hotmail.com>
2023-10-04 21:20:23 -0300

    testes de formatos de email, telefone, cpf

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -22,23 +22,91 @@ class UsuarioController {
         tipoUsuario,
       } = req.body;
 
-// Verifica se os campos obrigatórios estão ausentes ou vazios
-if (!enderecoId || !cep || !logradouro || !numero || !bairro || !cidade || !estado || //campos de endereço obrigatórios
-!nomeCompleto || !cpf || !dataNascimento || !telefone || !email || !senha || !tipoUsuario) { //campos de usuario obrigatórios
-    return res.status(422).json({ error: 'Todos os campos obrigatórios devem ser preenchidos.' });
-  }
+      // Verifica se os campos obrigatórios estão ausentes ou vazios
+      if (
+        !enderecoId ||
+        !cep ||
+        !logradouro ||
+        !numero ||
+        !bairro ||
+        !cidade ||
+        !estado || //campos de endereço obrigatórios
+        !nomeCompleto ||
+        !cpf ||
+        !dataNascimento ||
+        !telefone ||
+        !email ||
+        !senha ||
+        !tipoUsuario
+      ) {
+        //campos de usuario obrigatórios
+        return res.status(422).json({
+          error: "Todos os campos obrigatórios devem ser preenchidos.",
+        });
+      }
 
-  // Verifica se o email já está cadastrado
-  const emailExiste = await Usuario.findOne({ where: { email: email } });
-  if (emailExiste) {  
-      return res.status(409).json({ error: 'Email já cadastrado.' });
-  }
+      // Verifica se o email já está cadastrado
+      const emailExiste = await Usuario.findOne({ where: { email: email } });
+      if (emailExiste) {
+        return res.status(409).json({ error: "Email já cadastrado." });
+      }
 
-  // Verifica se o cpf já está cadastrado
-  const cpfExiste = await Usuario.findOne({ where: { cpf: cpf } });
-  if (cpfExiste) {
-      return res.status(409).json({ error: 'CPF já cadastrado.' });
-  }
+      // Verifica se o cpf já está cadastrado
+      const cpfExiste = await Usuario.findOne({ where: { cpf: cpf } });
+      if (cpfExiste) {
+        return res.status(409).json({ error: "CPF já cadastrado." });
+      }
+
+      if (!/^\S+@\S+\.\S+$/.test(email)) {
+        // Verifica o formato do email
+        return res
+          .status(400)
+          .json("O campo email está em um formato inválido.");
+      }
+      if (!/^\d{10}$/.test(telefone)) {
+        // Verifica o formato do telefone
+        return res
+          .status(400)
+          .json("O campo telefone está em um formato inválido.");
+      } else if (!/^[0-9]+$/.test(telefone)) {
+        // Verifica se há apenas números
+        return res
+          .status(400)
+          .json("O campo telefone deve conter apenas números.");
+      }
+
+      if (!/^\d{11}$/.test(cpf)) {
+        // Verifica o formato do cpf
+        return res.status(400).json("O campo cpf está em um formato inválido.");
+      } else if (!/^[0-9]+$/.test(cpf)) {
+        // Verifica se há apenas números
+        return res.status(400).json("O campo CPF deve conter apenas números.");
+      }
+
+      if (
+        senha.length < 8 ||
+        !/[a-z]/.test(senha) || // Pelo menos uma letra minúscula
+        !/[A-Z]/.test(senha) || // Pelo menos uma letra maiúscula
+        !/\d/.test(senha) || // Pelo menos um número
+        !/[@#$%^&+=]/.test(senha) // Pelo menos um caracter especial
+      ) {
+        return res
+          .status(400)
+          .json("O campo senha está em um formato inválido.");
+      }
+
+      //Adiciona o endereço no BD
+      const endereco = await Endereco.create({
+        cep,
+        logradouro,
+        numero,
+        bairro,
+        cidade,
+        estado,
+        complemento,
+        latitude,
+        longitude,
+      });
 
       const usuario = await Usuario.create({
         enderecoId,
@@ -53,8 +121,8 @@ if (!enderecoId || !cep || !logradouro || !numero || !bairro || !cidade || !esta
       });
 
       return res
-      .status(201)
-      .send({ data: "Usuario salvo com sucesso!", usuario: usuario });
+        .status(201)
+        .send({ data: "Usuario salvo com sucesso!", usuario: usuario });
     } catch (error) {
       return res.status(500).json(error.message);
     }
