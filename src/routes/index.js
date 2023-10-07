1cc7273ba5f8dec567c2a26d740b725d2a16fd2c
ricardo-werner <wernerdive@gmail.com>
2023-10-07 11:18:05 -0300

    feat: adicionado routesFromCarrinho

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
-const { routesFromVendas} = require('./vendas.routes')
+const { routesFromVendas } = require('./vendas.routes')
+const { routesFromCarrinho } = require('./carrinho.routes')
 
 
 const routes = Router()
 
 routes.use('/api', [
   routesFromVendas(),
+  routesFromCarrinho()
 
 ])
 
