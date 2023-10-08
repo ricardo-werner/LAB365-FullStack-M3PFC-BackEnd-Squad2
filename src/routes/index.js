aa0e272b27dd8c4fc986efef2d7d525fbc904aac
Rafael Zampieron <rafaelprimieri@hotmail.com>
2023-10-07 21:10:09 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const { routesFromUsuario } = require('./usuario.routes')
 const { routesFromVendas } = require('./vendas.routes')
 const { routesFromCarrinho } = require('./carrinho.routes')
 const { routesFromPedido } = require('./pedido.routes')
+const { routesFromProduto } = require('./produtos.routes');
 
 const routes = Router()
 
@@ -10,7 +11,8 @@ routes.use('/api', [
   routesFromUsuario(),
   routesFromVendas(),
   routesFromCarrinho(),
-  routesFromPedido()
+  routesFromPedido(),
+  routesFromProduto(),
 ])
 
 module.exports = routes
