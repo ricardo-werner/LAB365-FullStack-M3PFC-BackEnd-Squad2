3302dc212f3a90f998e3e17a001db966ace14332
ricardo-werner <wernerdive@gmail.com>
2023-10-09 02:48:50 -0300

    refactor: alterado para camelCase

--- src/models/carrinho.js
+++ src/models/carrinho.js
@@ -3,19 +3,31 @@ const sequelize = require('../config/database.config');
 const { connection } = require('../database/connection');
 
 const Carrinho = connection.define('carrinho', {
-    id: {
-        type: INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    produtoId: {
+    usuarioId: {
         type: INTEGER,
         allowNull: false,
         references: {
-            model: 'produtos',
+            model: 'usuarios',
             key: 'id',
         },
+    },
+    vendedorId: {
+        type: INTEGER,
+        allowNull: false,
+        references: {
+            model: 'usuarios',
+            key: 'id',
+        },
+    },
+    produtoId: {
+        type: ARRAY(INTEGER), // Para armazenar vários IDs de produtos
+        allowNull: false,
+        references: {
+            model: {
+                tableName: 'produtos'
+            },
+            key: 'id'
+        }
     },
     quantidade: {
         type: INTEGER,
