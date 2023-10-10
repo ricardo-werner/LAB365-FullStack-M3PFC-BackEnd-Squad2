a3b4ca9203582bb910cdd72e785155ffc082aea6
deborasous <deborapatriciadp27@gmail.com>
2023-10-09 21:05:55 -0300

    Controller comprador: removi os arquivos de comprador e movi os codigos para dentro dos arquivos de usuarios. Middleware: removi a verificação de usuario admin para o middleware checarAdmin.js, no Auth ficou a verificação do token e acrescentei uma verificação se o usuario que está tentando logan existe no banco. Usuarios: criei o controller listarEnderecosComprador e a rota, criei um metodo para recuperar senha

--- src/routes/produtos.routes.js
+++ src/routes/produtos.routes.js
@@ -1,5 +1,5 @@
-const { Router } = require("express");
-const { auth } = require("../middleware/auth");
+const { Router } = require('express');
+const { auth } = require('../middleware/auth');
 
 const {
   cadastrarProduto,
@@ -7,16 +7,16 @@ const {
   filtrarProdutos,
   detalharProduto,
   atualizarProduto,
-} = require("../controllers/produtos.controller");
+} = require('../controllers/produtos.controller');
 
 class ProdutosRoutes {
   routesFromProduto() {
     const produtosRoutes = Router();
-    produtosRoutes.post("/produtos/admin", auth, cadastrarProduto);
-    produtosRoutes.get("/produto/admin", listarProdutosAdmin);
-    produtosRoutes.get("/produto/:offset/:limit", auth, filtrarProdutos);
-    produtosRoutes.get("/produto/:produtoId", auth,  detalharProduto);
-    produtosRoutes.patch("/produto/admin/:produtoId", atualizarProduto);
+    produtosRoutes.post('/produtos/admin', cadastrarProduto);
+    produtosRoutes.get('/produto/admin', listarProdutosAdmin);
+    produtosRoutes.get('/produto/:offset/:limit', filtrarProdutos);
+    produtosRoutes.get('/produto/:produtoId', detalharProduto);
+    produtosRoutes.patch('/produto/admin/:produtoId', atualizarProduto);
     return produtosRoutes;
   }
 }
