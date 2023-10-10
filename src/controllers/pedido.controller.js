99134b0fa7b3f6aa464586536e0e7d1645dc91e6
ricardo-werner <wernerdive@gmail.com>
2023-10-09 23:38:36 -0300

    refactor: add item preco

--- src/controllers/pedido.controller.js
+++ src/controllers/pedido.controller.js
@@ -11,6 +11,7 @@ class pedidoController {
                 produtoId,
                 enderecoId,
                 quantidade,
+                preco,
                 valorTotal,
                 tipoPagamento,
                 status
